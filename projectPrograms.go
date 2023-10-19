a2c69284e417d14eeeb37d3f21d05a4aca43ad4e
sdb6442 <sdb6442@gmail.com>
2023-10-18 23:26:10 -0400

    Update projectPrograms.go
    added 2 implementations of int methods, 1 implementation of string method

--- projectPrograms.go
+++ projectPrograms.go
@@ -1,3 +1,10 @@
+// Term Project Assignment
+// We elected to create one program that satisfies the three programming concepts.
+// The following program uses the language 'Go' to create a deck of cards and manipulate the deck to satisfy
+// the assignment ruberic.
+// Group Accelerate: Brian Samuels, Samuel Baynes, Tristan Hall, Jasmine Kingg, Alex Johnston
+// Date: 10/19/2023
+
 // Program 1:
 //TODO:
 
@@ -10,8 +17,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	//"math"
-	//"string"
 )
 
 type cardMaker struct {
@@ -63,5 +70,43 @@ func newDeck() (deck Deck) {
 func main() {
 	deck := newDeck()
 
+	// Print deck of cards
+	fmt.Println("Deck of cards: ")
 	fmt.Println(deck)
+
+	// 8 Methods showcasing data manipulation
+
+	// Two int Methods:
+	// 1. Max card value when comparing two cards - max()
+	card1 := deck[1]
+	card2 := deck[13]
+	maxCard := max(card1.value, card2.value)
+	fmt.Println("\nCard 1: ", card1, " Card 2: ", card2, " Value of highest card: ", maxCard)
+
+	// 2. Min card value when comparing two cards - min()
+	minCard := min(card1.value, card2.value)
+	fmt.Println("\nCard 1: ", card1, " Card 2: ", card2, " Value of lowest card: ", minCard)
+
+	// Two String Methods:
+	// 1. Count the number of Heart cards by creating a string consisting of each occurance of
+	//    "Heart", then counting substrings of "Heart" using strings.Count() method
+	var heartString string
+	for i := 0; i < len(deck); i++ {
+		if deck[i].suit == "Heart" {
+			heartString += deck[i].suit
+		}
+	}
+	heartCount := strings.Count(heartString, "Heart")
+	fmt.Println("\nNumber of Heart cards: ", heartCount)
+
+	// 2.
+
+	// Two Boolean Methods:
+	// 1.
+	// 2.
+
+	// Two Rune Methods:
+	// 1.
+	// 2.
+
 }
